e8239620628cbe2ceb07e457928b949b821608b0
Unknown <valentin.leon0@gmail.com>
2018-04-05 20:07:45 +0200

    Purge de code

--- js/main.js
+++ js/main.js
@@ -1,5 +1,46 @@
 $.noConflict();
 
+jQuery(window).ready(function () {
+
+            function go_to(id_div = 'index', page_title = 'CampusID', breadcrumb = null) {
+                let step1 = '';
+                let step2 = '';
+
+                if (breadcrumb) {
+                    if (breadcrumb.step1 !== undefined) {
+                        step1 = breadcrumb.step1;
+                    }
+                    if (breadcrumb.step2 !== undefined) {
+                        step2 = breadcrumb.step2;
+                    }
+                }
+                else {
+                    step1 = 'Bienvenu sur l\'IDBoard';
+                }
+
+                $('#fst_step').text(step1);
+                $('#snd_step').text(step2);
+
+                $('.right-panel .content').css('display', 'none');
+                $('#' + id_div).css('display', 'block');
+                $('title').text(page_title);
+            }
+
+            go_to();
+
+            $('.menu-link').click(function () {
+                go_to(
+                    $(this).attr('data-id'),
+                    $(this).attr('data-page-title'),
+                    {
+                        step1: $(this).attr('data-breadcrumb-step1'),
+                        step2: $(this).attr('data-breadcrumb-step2')
+                    }
+                );
+            });
+
+        });
+
 jQuery(document).ready(function($) {
 
 	"use strict";
